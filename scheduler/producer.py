7be3b45e91deace452f5c4ebb525bd14dabf50ee
jazzpu <jasmine.theseira@gmail.com>
2025-09-02 16:49:08 +0700

    Part 8: Scheduler and database rabbit MQ

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,12 +1,16 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
+def produce(host, body):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    channel = connection.channel()
 
-channel.exchange_declare(exchange="jobs", exchange_type="direct")
-channel.queue_declare(queue="router_jobs")
-channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    channel.queue_declare(queue="router_jobs")
+    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
 
-channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body="192.168.1.44")
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
 
-connection.close()
+    connection.close()
+
+if __name__ == "__main__":
+    produce("localhost", "192.168.1.44")
