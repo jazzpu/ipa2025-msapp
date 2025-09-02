4b5f84a36f39a52572b38cbf4c70316f50c38bff
jazzpu <jasmine.theseira@gmail.com>
2025-09-02 15:26:16 +0700

    adding credentials for database

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ipa2025-msapp
 this is a github repository about docker composing
+
+### How to set-up your environment?
+- sudo apt update
+- sudo apt install python3-pip
+- python3 -m pip install -r requirements.txt
+- dont forget to configure env file for future credentials.
+
+### If you want to change the username password don't forget to down/up your docker compose
+- docker compose down --volumes
+![Compose down](images/docker_composedown.png)
--- web/app.py
+++ web/app.py
@@ -1,38 +1,40 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import redirect
-from flask import url_for
+import os
+
+from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
-client = MongoClient("mongodb://mongo:27017/")
-mydb = client["mydatabase"]
-mycol = mydb["mycollection"]
 
-@app.route("/")
-def main():
-    return render_template("index.html", data=mycol.find())
+mongo_uri  = os.environ.get("MONGO_URI")
+db_name    = os.environ.get("DB_NAME")
+
+client = MongoClient(mongo_uri)
+db = client[db_name]
+routers = db["routers"]
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", routers=list(routers.find()))
 
 @app.route("/add", methods=["POST"])
-def add_comment():
+def add_router():
     ip = request.form.get("ip")
     username = request.form.get("username")
-    passwd = request.form.get("passwd")
+    password = request.form.get("passwd")
 
-    if ip and username and passwd:
-        mycol.insert_one({"ip": ip, "username":username, "passwd":passwd})
-    return redirect(url_for("main"))
+    if ip and username and password:
+        routers.insert_one({
+            "ip": ip,
+            "username": username,
+            "password": password
+        })
+    return redirect("/")
 
-@app.route("/delete", methods=["POST"])
-def delete_comment():
-    try:
-        idx = int(request.form.get("idx"))
-        if 0 <= idx < len(mycol.find()):
-            mycol.delete_one(idx)
-    except Exception:
-        pass
-    return redirect(url_for("main"))
+@app.route("/delete/<id>", methods=["POST"])
+def delete_router(id):
+    routers.delete_one({"_id": ObjectId(id)})
+    return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
